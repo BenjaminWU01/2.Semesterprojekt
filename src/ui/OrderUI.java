aebccb78862344cc569b407bbeaa7514469e210e
viggo-vampyr <93257309+viggo-vampyr@users.noreply.github.com>
2022-05-10 09:22:27 +0200

    OrderUI

--- src/ui/OrderUI.java
+++ src/ui/OrderUI.java
@@ -2,4 +2,20 @@ package ui;
 
 public class OrderUI {
 
+	public void registerOrder() {
+
+	}
+
+	public void addProduct() {
+
+	}
+
+	public void addCustomer() {
+
+	}
+
+	public void completeOrder() {
+
+	}
+
 }
