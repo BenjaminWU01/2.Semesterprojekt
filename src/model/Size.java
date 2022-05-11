f879b92a06a6867dc0e65e7a9f846a0c534a84dc
MathiasBjoerkmann <10407511@ucn.dk>
2022-05-11 13:03:15 +0200

    ui

--- src/model/Size.java
+++ src/model/Size.java
@@ -2,4 +2,20 @@ package model;
 
 public class Size {
 
+	private String sizeDesc;
+
+	public Size(String sizeDesc) {
+		super();
+		this.sizeDesc = sizeDesc;
+	}
+
+	public String getSizeDesc() {
+		return sizeDesc;
+	}
+
+	public void setSizeDesc(String sizeDesc) {
+		this.sizeDesc = sizeDesc;
+	}
+	
+	
 }
