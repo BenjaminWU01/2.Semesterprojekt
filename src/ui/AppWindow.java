3edd3800df2bed3de48eae64adb973829d641dbc
viggo-vampyr <93257309+viggo-vampyr@users.noreply.github.com>
2022-05-16 10:34:34 +0200

    addItem

--- src/ui/AppWindow.java
+++ src/ui/AppWindow.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import db.DataAccessException;
+import model.Size;
 
 import javax.swing.JTextField;
 import javax.swing.JTable;
@@ -17,10 +18,15 @@ import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JSpinner;
+import javax.swing.SwingConstants;
+import javax.swing.DefaultComboBoxModel;
 
 public class AppWindow extends JFrame {
 
@@ -32,6 +38,9 @@ public class AppWindow extends JFrame {
 	private JButton btnNewButton_3;
 	private JButton btnNewButton_4;
 	private OrderWindow ow;
+	private OrderUI oUI;
+	private JTextField textQty;
+	private JComboBox comboBoxSize;
 
 	/**
 	 * Launch the application.
@@ -64,6 +73,12 @@ public class AppWindow extends JFrame {
 		contentPane.setLayout(null);
 
 		txtSearchForProduct = new JTextField();
+		txtSearchForProduct.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtSearchForProduct.setText("");
+			}
+		});
 		txtSearchForProduct.setText("Search for product");
 		txtSearchForProduct.setBounds(34, 88, 704, 43);
 		contentPane.add(txtSearchForProduct);
@@ -87,6 +102,17 @@ public class AppWindow extends JFrame {
 		scrollPane.setViewportView(table);
 
 		btnNewButton_1 = new JButton("Add");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					addItem();
+				} catch (SQLException e1) {
+					System.out.println("Couldnt add item to order");
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnNewButton_1.setBounds(34, 260, 171, 59);
 		contentPane.add(btnNewButton_1);
@@ -111,7 +137,52 @@ public class AppWindow extends JFrame {
 		lblNewLabel.setBounds(389, 29, 200, 43);
 		contentPane.add(lblNewLabel);
 
+		textQty = new JTextField();
+		textQty.setText("1");
+		textQty.setBounds(34, 171, 96, 19);
+		contentPane.add(textQty);
+		textQty.setColumns(10);
+
+		JLabel lblQty = new JLabel("Quantity");
+		lblQty.setBounds(34, 157, 47, 15);
+		contentPane.add(lblQty);
+
+		JLabel lblSize = new JLabel("Size");
+		lblSize.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSize.setBounds(365, 154, 60, 19);
+		contentPane.add(lblSize);
+
+		comboBoxSize = new JComboBox();
+		comboBoxSize.setModel(new DefaultComboBoxModel(new String[] { "Small", "Medium", "Large" }));
+		comboBoxSize.setBounds(314, 170, 108, 21);
+		contentPane.add(comboBoxSize);
+
+		oUI = new OrderUI();
+		oUI.registerOrder();
 		openWindow();
+	}
+
+	protected void addItem() throws SQLException {
+		Size s = new Size(comboBoxSize.getActionCommand());
+		if (checkInt() != false) {
+			int x = Integer.parseInt(textQty.getText());
+			oUI.addProduct(txtSearchForProduct.getText(), x, s);
+		} else {
+			System.out.println("The input in Quantity must be a number");
+		}
+
+	}
+
+	public boolean checkInt() {
+		boolean result = false;
+		try {
+			int x = Integer.parseInt(textQty.getText());
+			result = true;
+		} catch (NumberFormatException e) {
+			System.out.println("The input in Quantity must be a number");
+		}
+		return result;
+
 	}
 
 	public void openWindow() throws DataAccessException {
