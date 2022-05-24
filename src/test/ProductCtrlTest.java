5570e331fd20c2b4134e221f7a492382a107fa8e
Morten M. Andersen <93257440+Morten-M-Andersen@users.noreply.github.com>
2022-05-24 17:04:29 +0200

    <half-ass ProductCtrl JUnit4 Test
    <half-ass ProductCtrl JUnit4 Test

---
+++ src/test/ProductCtrlTest.java
@@ -0,0 +1,60 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.ProductCtrl;
+import db.DataAccessException;
+import model.Product;
+import model.Size;
+
+public class ProductCtrlTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void Calling_getProductWithCorrectProdNoAndSize_ShouldReturn_MatchingProductObject() {
+		//Arrange
+		ProductCtrl prodCtrl = new ProductCtrl();
+		
+		String testSizeDesc = "Small";
+		Size testSize = new Size(testSizeDesc, 1);
+		String testProdNo = "115";
+		
+		Product returnedProduct;
+		
+		//Act
+		returnedProduct = prodCtrl.getProduct(testProdNo, testSize);
+		
+		//Assert
+		assertTrue("", returnedProduct.getProdNo().equals(testProdNo) && returnedProduct.getSize().getSizeDesc().equals(testSizeDesc));
+	}
+	
+//	@Test (expected = DataAccessException.class)
+//	public void Calling_getProductWithIncorrectProdNoAndSize_ShouldReturn_MatchingProductObject() {
+//		//Arrange
+//		ProductCtrl prodCtrl = new ProductCtrl();
+//		
+//		String testSizeDesc = "Small";
+//		Size testSize = new Size(testSizeDesc, 1);
+//		String testProdNo = "9797979";
+//		
+//		Product returnedProduct;
+//		
+//		//Act
+//		returnedProduct = prodCtrl.getProduct(testProdNo, testSize);
+//		
+//		//Assert
+//		//assertThrows(DataAccessException.class, );
+//	}
+
+}
